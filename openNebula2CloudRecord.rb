5a282111f1bbf9385faa92b8fc83d9b3556a88bb
Andrea Guarise <andrea.guarise@to.infn.it>
2013-03-27 17:17:19 +0100

    Improved record and output

--- openNebula2CloudRecord.rb
+++ openNebula2CloudRecord.rb
@@ -22,12 +22,28 @@ class OpenNebulaJsonRecord
   def recordVector
     rv = {}
     #rv['FQAN'] = @jsonRecord['a']
-    rv['VMUUID'] = @jsonRecord["VM"]["ID"]
     rv['cloudType'] = "OpenNebula"
-    rv['cpuCount'] = @jsonRecord["VM"]["TEMPLATE"]["CPU"]
+    if @jsonRecord["VM"]["TEMPLATE"]["CPU"] then
+	#Number of physical CPU was assigned in the template. Use this    
+	rv['cpuCount'] = @jsonRecord["VM"]["TEMPLATE"]["CPU"]
+    else
+	#Number of physical CPU was not assigned in the template, just Virtual CPUS 
+	#Where requested. This causes possible overbooking. Use this if physical is
+	#not specified
+	rv['cpuCount'] = @jsonRecord["VM"]["TEMPLATE"]["VCPU"]
+    end
     #rv['cpuDuration'] = @jsonRecord["VM"]
     #rv['Disk'] = @jsonRecord['e']
-    rv['diskImage'] = @jsonRecord["VM"]["TEMPLATE"]["DISK"]["IMAGE"]
+    if @jsonRecord["VM"]["TEMPLATE"]["DISK"]
+	if @jsonRecord["VM"]["TEMPLATE"]["DISK"].kind_of?(Array)
+		rv['diskImage'] = ""
+    		@jsonRecord["VM"]["TEMPLATE"]["DISK"].each do |disk|
+			rv['diskImage'] += disk["IMAGE"] if disk["IMAGE"] 
+		end
+    	else
+		rv['diskImage'] = @jsonRecord["VM"]["TEMPLATE"]["DISK"]["IMAGE"] if @jsonRecord["VM"]["TEMPLATE"]["DISK"]["IMAGE"]
+    	end
+    end
     rv['endTime'] = Time.at(@jsonRecord["ETIME"].to_i).to_datetime
     #rv['globaluserName'] = @jsonRecord["e"]
     rv['localVMID'] = @jsonRecord["VM"]["ID"]
@@ -38,7 +54,10 @@ class OpenNebulaJsonRecord
     rv['networkOutBound'] = @jsonRecord["VM"]["NET_TX"]
     #rv['networkType'] = @jsonRecord['q']
     #rv['resource_name'] = @resourceName
-    rv['startTime'] = Time.at(@jsonRecord["STIME"].to_i).to_datetime
+    timeBuff = @jsonRecord["RSTIME"] if @jsonRecord["RSTIME"] != "0"
+    timeBuff = @jsonRecord["PSTIME"] if @jsonRecord["PSTIME"] != "0"
+    timeBuff = @jsonRecord["STIME"] if @jsonRecord["STIME"] != "0"
+    rv['startTime'] = Time.at(timeBuff.to_i).to_datetime
     rv['status'] = @jsonRecord['VM']['STATE'] + ":" + @jsonRecord['VM']['LCM_STATE']
     #rv['storageRecordId'] = @jsonRecord['u']
     #rv['suspendDuration'] = @jsonRecord['v']
@@ -46,20 +65,23 @@ class OpenNebulaJsonRecord
 	@jsonRecord["ETIME"] = dateTime = Time.new.to_time.to_i
     end
     rv['wallDuration'] = @jsonRecord["ETIME"].to_i - @jsonRecord["STIME"].to_i
+    rv['VMUUID'] = @resourceName  + "-" + @jsonRecord["STIME"] + "-" +@jsonRecord["VM"]["ID"]
     rv
   end
   
   def to_s
     stringVector = "VMUUID = " + self.recordVector['VMUUID'] + "\n"
-    stringVector += "cloudType = " + self.recordVector['cloudType'] + "\n"
-    stringVector += "cpuCount = " + self.recordVector['cpuCount'] + "\n"
     stringVector += "startTime = " + self.recordVector['startTime'].to_s + "\n"
     stringVector += "endTime = " + self.recordVector['endTime'].to_s + "\n"
   end
 
-  #def resourceName=(resourceName)
-#	@resourceName = resourceName
- # end
+  def resourceName=(resourceName)
+	@resourceName = resourceName
+  end
+
+  def resourceName
+	@resourceName
+  end
 
 end
 
@@ -103,8 +125,13 @@ end
 CloudRecord.site = options[:uri]
 CloudRecord.headers['Authorization'] = "Token token=\"#{options[:token]}\""
 CloudRecord.timeout = 5
-#EmiComputeAccountingRecord.proxy = ""
-
+CloudRecord.proxy = ""
+  
+  if values['resource_name'] then
+    ResourceName=values['resource_name']
+  else
+    ResourceName=ENV["HOSTNAME"]
+  end
   threads = values['threads'].to_i + 1
   thread_counter = 0
   $stdout.sync = true
@@ -117,21 +144,18 @@ CloudRecord.timeout = 5
     i = 0
     end
     Thread.new {
-      puts JSON.pretty_generate(jsonRecord)
+      puts JSON.pretty_generate(jsonRecord) if options[:verbose]
       record = OpenNebulaJsonRecord.new(jsonRecord)
-      puts record.to_s
+      record.resourceName = ResourceName 
+      puts record.to_s 
       r = CloudRecord.new(record.recordVector)
-      if values['resource_name'] then
-        r.resource_name=values['resource_name']
-      else
-        r.resource_name=ENV["HOSTNAME"]
-      end
+      r.resource_name=ResourceName
       tries = 0
       begin
         tries += 1
 	r.save
 	if not r.valid? 
-          puts r.errors.full_messages
+          puts r.errors.full_messages if options[:verbose]
 	  recordBuff = CloudRecord.get(:search, :VMUUID => r.VMUUID )
 	  newRecord = CloudRecord.find(recordBuff["id"])
 	  newRecord.load(r.attributes)
@@ -139,10 +163,12 @@ CloudRecord.timeout = 5
 	end
 
       rescue Exception => e
-        puts "Error sending  #{r.VMUUID}:#{e.to_s}. Retrying"
-        if ( tries < 3)
+        puts "Error sending  #{r.VMUUID}:#{e.to_s}. Retrying" if options[:verbose]
+        if ( tries < 2)
           sleep(2**tries)
           retry
+	else
+		puts "Could not send record #{r.VMUUID}."
         end
       end
     }
