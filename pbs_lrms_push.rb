8ca6f4ba0362c99e5352c032f3878e6d3120639a
Andrea Guarise <andrea.guarise@to.infn.it>
2013-05-07 11:28:53 +0200

    start fetching records from sqlite

--- pbs_lrms_push.rb
+++ pbs_lrms_push.rb
@@ -3,7 +3,7 @@ require 'rubygems'
 require 'optparse'
 require 'active_resource'
 require 'json'
-require 'date'
+require 'sqlite3'
 
 options = {}
   
@@ -11,3 +11,40 @@ class GenericResource < ActiveResource::Base
   self.format = :xml
 end
 
+
+opt_parser = OptionParser.new do |opt|
+  opt.banner = "Usage: record_post [OPTIONS] field=value ..."
+
+  options[:verbose] = false
+  opt.on( '-v', '--verbose', 'Output more information') do
+    options[:verbose] = true
+  end
+
+  options[:uri] = nil
+  opt.on( '-U', '--URI uri', 'URI to contact') do |uri|
+    options[:uri] = uri
+  end
+
+  options[:token] = nil
+  opt.on( '-t', '--token token', 'Authorization token that must be obtained from the service administrator') do |token|
+    options[:token] = token
+  end
+
+  options[:database] = nil
+  opt.on( '-d', '--database file', 'file containing the sqlite record db') do |token|
+    options[:database] = token
+  end
+
+  opt.on( '-h', '--help', 'Print this screen') do
+    puts opt
+    exit
+  end
+end
+
+opt_parser.parse!
+
+db = SQLite3::Database.new options[:database]
+
+db.execute( "select * from records" ) do |row|
+  p row
+end
