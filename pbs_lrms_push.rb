088ba85495fa825e05d35c6cc643c62496b4b070
guarise <andrea.guarise@to.infn.it>
2013-05-08 11:20:41 +0200

    First working active resource implementation

--- pbs_lrms_push.rb
+++ pbs_lrms_push.rb
@@ -18,7 +18,7 @@ class Record
   def initialize(row)
       @row = row
   end
-   
+
   def to_hash
     rh = {}
     rh['user'] = @row['user']
@@ -26,7 +26,7 @@ class Record
     rh['lrmsId'] = @row['lrmsId']
     rh['queue'] = @row['queue']
     rh['resourceUsed_cput'] = @row['cput']
-    rh['resourceused_walltime'] = @row['walltime']
+    rh['resourceUsed_walltime'] = @row['walltime']
     rh['resourceUsed_vmem'] = @row['vmem']
     rh['resourceUsed_mem'] = @row['mem']
     #rh[''] = @row['processors']
@@ -39,8 +39,8 @@ class Record
     rh['end'] = @row['end']
     rh['execHost'] = @row['execHost']
     rh['exitStatus'] = @row['exitStatus']
-    rh    
-  end 
+    rh
+  end
 end
 
 opt_parser = OptionParser.new do |opt|
@@ -79,6 +79,11 @@ end
 
 opt_parser.parse!
 
+TorqueExecuteRecord.site = options[:uri]
+TorqueExecuteRecord.headers['Authorization'] = "Token token=\"#{options[:token]}\""
+TorqueExecuteRecord.timeout = 5
+TorqueExecuteRecord.proxy = ""
+
 #open the database connection
 db = SQLite3::Database.new options[:database]
 db.results_as_hash = true;
@@ -92,9 +97,31 @@ while ( start < stop )
   rs.each do |row|
     jsonRecord = JSON.parse(row['record'])
     p "KEY: #{row["key"]},#{jsonRecord["lrmsId"]}"
-    recordBuff Record.new(jsonRecord)
+    recordBuff = Record.new(jsonRecord)
+    r = TorqueExecuteRecord.new(recordBuff.to_hash)
+    tries = 0
+    begin
+      tries += 1
+      r.save
+      if not r.valid?
+        puts r.errors.full_messages if options[:verbose]
+        #oldRecord = TorqueExecuteRecord.get(:search, :VMUUID => r.VMUUID )
+        #newRecord = TorqueExecuteRecord.find(oldRecord["id"])
+        #newRecord.load(r.attributes)
+        #newRecord.save
+      end
+
+    rescue Exception => e
+      puts "Error sending  #{r.lrmsId}:#{e.to_s}. Retrying" if options[:verbose]
+      if ( tries < 2)
+        sleep(2**tries)
+        retry
+      else
+        puts "Could not send record #{r.lrmsId}."
+      end
+    end
   end
   start = start.to_i + options[:number].to_i;
 
-
 end
+
