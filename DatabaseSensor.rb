896bfd4f3fd1d4b4d9ee0fc003dac5506fd8803b
Andrea Guarise <andrea.guarise@to.infn.it>
2014-01-30 11:34:06 +0100

    working hard on

--- DatabaseSensor.rb
+++ DatabaseSensor.rb
@@ -10,9 +10,46 @@ class DatabaseRecord
   
 end
 
+class Table
+  def initialize(name)
+    @name = name
+    @rows = 0 #number of rows
+    @avg_row_length = 0 #set in bytes, avg bytes per row
+    @data_length = 0 #set in bytes, data file dimensions
+  end
+  
+  def to_s
+    "name:#{@name}|rows:#{@rows}|avg_row_length:#{@avg_row_length}|data_length:#{@data_length}"
+  end
+  
+  def rows=(rows)
+    @rows = rows
+  end
+  
+  def rows
+    @rows
+  end
+  
+  def avg_row_length=(a)
+    @avg_row_length=a
+  end
+  
+  def data_length=(d)
+    @data_length=d
+  end
+    
+end
+
 class Database
-  def initialize(dbContactString)
+  def initialize(dbContactString,user,password)
     @dbContactString = dbContactString
+    pattern = /^(.*):(.*):(.*)$/
+    pattern =~ dbContactString
+    data = Regexp.last_match
+    @dbName = data[2]
+    @user = user
+    @password = password
+    @dbh = nil
   end
   
   def to_s
@@ -21,20 +58,46 @@ class Database
   
   def connect
     begin
-      dbh = DBI.connect("DBI:#{@dbContactString}", 
-                      "root", "z1b1bb0")
-         # get server version string and display it
-      row = dbh.select_one("SELECT VERSION()")
-      puts "Server version: " + row[0]
+      @dbh = DBI.connect("DBI:#{@dbContactString}", 
+                      @user, @password)
+         #  
     rescue DBI::DatabaseError => e
       puts "An error occurred"
       puts "Error code:    #{e.err}"
       puts "Error message: #{e.errstr}"
-    ensure
-      # disconnect from server
-      dbh.disconnect if dbh
     end
   end
+  
+  def disconnect 
+    @dbh.disconnect if @dbh
+  end
+  
+  def getTables
+    @dbh.tables
+  end
+end
+
+class MySQL < Database
+  
+  def getVersion
+     #get server version string and display it
+     row = @dbh.select_one("SELECT VERSION()")
+     row[0]
+  end
+  
+  def getTableInfo
+     result = @dbh.select_all("SHOW TABLE STATUS IN #{@dbName}")
+     result.each do |row|
+       t = Table.new(row[0])
+       t.rows=row[4]
+       t.avg_row_length=row[5]
+       t.data_length=row[6]
+       puts t.to_s
+     end
+     #table = new Table(t)
+  end
+  
+  
 end
 
 class DatabaseSensor
@@ -57,9 +120,14 @@ class DatabaseSensor
         @options[:dryrun] = true
       end
   
-      @options[:publishers] = false
-      opt.on( '-p', '--publishers', 'Output retrieved publishers') do
-        @options[:publishers] = true
+      @options[:user] = nil
+      opt.on( '-u', '--user user', 'Database user') do |user|
+        @options[:user] = user
+      end
+  
+      @options[:password] = nil
+      opt.on( '-p', '--password password', 'Database password') do |password|
+        @options[:password] = password
       end
   
       @options[:uri] = nil
@@ -87,11 +155,27 @@ class DatabaseSensor
     opt_parser.parse!
   end
   
+  def getDbType
+    "MySQL"
+  end
+  
   def main
-    puts "Hello my dear!"
     self.getLineParameters
-    db = Database.new(@options[:db])
+    db = case
+      when  self.getDbType == "MySQL" then 
+         MySQL.new(@options[:db],@options[:user],@options[:password])
+      when  self.getDbType == "sqlite" then 
+        nil
+      else 
+        nil
+    end     
     db.connect
+    puts db.getVersion
+    db.getTables.each do |table|
+      puts table
+    end
+    db.getTableInfo
+    db.disconnect
     puts db.to_s
   end
 end
